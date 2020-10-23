af5cf1eb8fcdfd34bbd7582c99ba2f40a7394001
X-Green <2271174177@qq.com>
2020-10-23 21:32:49 +0800

    first

--- src/JSParser.java
+++ src/JSParser.java
@@ -1,5 +1,8 @@
+import java.io.File;
+
 public class JSParser {
     public static void main(String[] args) {
         System.out.println(233);
+        System.out.println(new File("").getAbsolutePath());
     }
 }
---
+++ test/test.js
@@ -0,0 +1,22 @@
+print("Hello world")
+aaa()
+// say something unimportant
+/* again
+and again
+*/
+
+function aaa() {
+    for(i = 0; i< 10; i++) {
+        print("qwq"+ i + "xxsxzsdhkgnzabx")
+    }
+    bbb(2)
+    bbb(3)
+}
+
+function bbb(i) {
+    if (i == 3) {
+        print("reeee")
+    } else{
+        print("rue")
+    }
+}
